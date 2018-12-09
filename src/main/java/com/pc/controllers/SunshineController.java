934ae81fa25f03c89dafe0ae27bcec57349a2929
Prashant <prashantchaubey9795@gmail.com>
2018-12-09 11:51:31 +0530

    added sunshine page

---
+++ src/main/java/com/pc/controllers/SunshineController.java
@@ -0,0 +1,22 @@
+package com.pc.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created By: Prashant Chaubey
+ * Created On: 09-12-2018 11:04
+ **/
+@Controller
+@RequestMapping("/sunshine")
+public class SunshineController {
+
+    private static Logger LOGGER = Logger.getLogger(SunshineController.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home() {
+        return "sunshine";
+    }
+}
---
+++ web/resources/js/sunshine.js
@@ -0,0 +1,27 @@
+var counter = 0;
+$(document).on('click', '#start', function () {
+    $(this).attr("hidden", true);
+    $("#poem").attr("hidden", false);
+    $("#show").attr("hidden", false);
+})
+
+$(document).on('click', "#show", function () {
+    console.log("counter:" + counter);
+    if (counter === 0) {
+        $(this).text("PAKKA!!!");
+        counter++;
+    } else if (counter === 1) {
+        $(this).text("NO CHEATING!!!");
+        counter++;
+    } else if (counter === 2) {
+        $(this).text("MEHNAT LAGI THI, PADH LO!!!");
+        counter++;
+    } else if (counter === 3) {
+        $(this).text("OKAY, GO ON!!!");
+        counter++;
+    } else if (counter === 4) {
+        $('.space').attr("hidden", false);
+        $(".firstWord").css('color', '#f26968');
+    }
+
+})
