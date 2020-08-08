5422588362b939d3a9e5d432615650c52d5755c8
Prashant <prashantchaubey9795@gmail.com>
2020-08-08 16:55:33 +0100

    Made CORS only for development profile

--- dev_notes.md
+++ dev_notes.md
@@ -5,3 +5,10 @@
 @Getter
 ```
 These 4 are used on entities instead of `@Value` to work with JPA.
+
+#
+
+Make sure that your main class is in a root package above other classes.
+
+When you run a Spring Boot Application, (i.e. a class annotated with `@SpringBootApplication`), Spring will only scan 
+the classes below your main class package.
--- src/main/java/com/prashantchaubey/Main.java
+++ src/main/java/com/prashantchaubey/Main.java
@@ -2,19 +2,27 @@ package com.prashantchaubey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class Main implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-    }
-
+public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    @Profile("dev")
+    public WebMvcConfigurer corsMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
