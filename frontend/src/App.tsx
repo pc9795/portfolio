b7b3356e434516d483403ea866a5ac7a914dd102
Prashant <prashantchaubey9795@gmail.com>
2020-08-30 13:06:18 +0100

    Moved navigation logic to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,36 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import SunshinePage from "./components/pages/SunshinePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ListsPage from "./components/pages/ListsPage";
+import BlogPostPage from "./components/pages/BlogPostPage";
+import OAuth2RedirectHandler from "./components/OAuth2RedirectHandler";
+import {AppRoutes} from "./utils/constants";
+import BlogPage from "./components/pages/BlogPage";
+import ContactPage from "./components/pages/ContactPage";
+import HomePage from "./components/pages/HomePage";
+import WorkPage from "./components/pages/WorkPage";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
-    return <Router>
-        <Switch>
-            <Route exact path="/sunshine" component={SunshinePage}/>
-            <Route path="/">
-                <Header/>
-                <Footer/>
-            </Route>
-        </Switch>
-    </Router>;
+    return <Switch>
+        <Route exact path="/sunshine" component={SunshinePage}/>
+        <Route path="/">
+            <Header/>
+            <Switch>
+                <Route exact path={AppRoutes.HOME} component={HomePage}/>
+                <Route exact path={AppRoutes.BLOG} component={BlogPage}/>
+                <Route exact path={AppRoutes.WORK} component={WorkPage}/>
+                <Route exact path={AppRoutes.LISTS} component={ListsPage}/>
+                <Route exact path={AppRoutes.CONTACT} component={ContactPage}/>
+                <Route exact path={AppRoutes.BLOG + "/:name"} component={BlogPostPage}/>
+                <Route exact path={AppRoutes.OAUTH2_REDIRECT} component={OAuth2RedirectHandler}/>
+                <Route component={NotFound}/>
+            </Switch>
+            <Footer/>
+        </Route>
+    </Switch>
 }
 
 export default App;
